2deb0d9f217dd3600b502c2fd35c0729e1f1ff42
Taki Uddin <msiam18@gmail.com>
2023-04-02 11:54:11 +0600

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Web & Media Team on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
